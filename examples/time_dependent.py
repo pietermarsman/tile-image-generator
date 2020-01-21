e644b4b32425b3f8f373e90e40d2e93117a17727
Pieter Marsman <pietermarsman@gmail.com>
2020-01-21 21:05:57 +0100

    Save image to desktop

--- examples/time_dependent.py
+++ examples/time_dependent.py
@@ -6,5 +6,5 @@ from generate import generate_hierarchical_tile_image
 time = datetime.now()
 day_percentage = time.hour / 24 + time.minute / 60 / 24
 image = generate_hierarchical_tile_image((3840, 2160), 5, 0.0, 1.0, 0.7, 1.0, 0.0, 1.0 - day_percentage)
-with open(os.path.expanduser('~/Pictures/background/time-dependent-background.png'), 'wb') as fout:
+with open(os.path.expanduser('~/Desktop/time-dependent-background.png'), 'wb') as fout:
     image.save(fout, 'PNG')
